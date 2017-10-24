cc21881458f32dcd0284e80774551471c2a7851e
David L. King <king.david.lee@gmail.com>
2017-10-24 15:22:51 -0400

    forgot some stuff with argparse

--- template.py
+++ template.py
@@ -9,7 +9,9 @@ class template:
         print("Sample usage: python3 randomSample.py filtered.tsv 0.1 0.2")
         parser = argparse.ArgumentParser()
         parser.add_argument('-f', '--folds_dir', help="folds directory (e.g. folds/gold")
-
+        requiredNamed = parser.add_argument_group('required named arguments')
+        requiredNamed.add_argument('-i', '--input', help='Input file name', required=True)
+        self.args = parser.parse_args()
 
     def load_file(self, wlfile):
         pass
